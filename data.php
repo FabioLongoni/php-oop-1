4d1e82bde24febae1b19c75611c90cced2a9cd0e
FabioLongoni <longoni.fabio90@gmail.com>
2022-09-26 16:33:33 +0200

    add-foreach-

--- data.php
+++ data.php
@@ -2,28 +2,26 @@
 
 include __DIR__.'/movie.php';
 
+$movies = 
+[
+  $firstMovie = new Movie('https://mr.comingsoon.it/imgdb/locandine/big/45068.jpg',
+                'The Wolf of Wall Street',
+                'The Wolf of Wall Street è un film di genere drammatico del 2013, diretto da Martin Scorsese, con Leonardo DiCaprio e Jonah Hill. Uscita al cinema il 23 gennaio 2014. Durata 179 minuti. Distribuito da 01 Distribution.',
+                2014,
+                'Drammatico',
+                179),
+  $secondMovie = new Movie('https://scale.coolshop-cdn.com/product-media.coolshop-cdn.com/AE49RZ/86269df3a5da4519975656c7b9a334a2.jpg/f/inglourious-basterds-quentin-tarantino-dvd.jpg',
+                'Inglourious Basterds',
+                'Bastardi senza gloria è un film di genere guerra, avventura, azione del 2009, diretto da Quentin Tarantino, con Brad Pitt e Diane Kruger. Uscita al cinema il 02 ottobre 2009. Durata 148 minuti. Distribuito da Universal Pictures.',
+                2009,
+                'Guerra, Avventura',
+                148),
+  $thirdMovie = new Movie('https://pad.mymovies.it/filmclub/2019/07/031/locandina.jpg',
+                'The Gentlemen',
+                'The Gentlemen è un film di genere azione, commedia del 2020, diretto da Guy Ritchie, con Matthew McConaughey e Hugh Grant. Durata 113 minuti. Distribuito da 01 Distribution, Amazon Prime Video.',
+                2020,
+                'Azione, Commedia',
+                113),                       
+];
 
-$firstMovie = new Movie('https://mr.comingsoon.it/imgdb/locandine/big/45068.jpg',
-                        'The Wolf of Wall Street',
-                        'The Wolf of Wall Street è un film di genere drammatico del 2013, diretto da Martin Scorsese, con Leonardo DiCaprio e Jonah Hill. Uscita al cinema il 23 gennaio 2014. Durata 179 minuti. Distribuito da 01 Distribution.',
-                        2013,
-                        'Drammatico',
-                        179);
-//var_dump($firstMovie);
-
-$secondMovie = new Movie('https://scale.coolshop-cdn.com/product-media.coolshop-cdn.com/AE49RZ/86269df3a5da4519975656c7b9a334a2.jpg/f/inglourious-basterds-quentin-tarantino-dvd.jpg',
-                         'Inglourious Basterds',
-                         'Bastardi senza gloria è un film di genere guerra, avventura, azione del 2009, diretto da Quentin Tarantino, con Brad Pitt e Diane Kruger. Uscita al cinema il 02 ottobre 2009. Durata 148 minuti. Distribuito da Universal Pictures.',
-                         2009,
-                         'Guerra, Avventura',
-                         148);
-//var_dump($secondMovie);
-
-$thirdMovie = new Movie('https://pad.mymovies.it/filmclub/2019/07/031/locandina.jpg',
-                         'The Gentlemen',
-                         'The Gentlemen è un film di genere azione, commedia del 2020, diretto da Guy Ritchie, con Matthew McConaughey e Hugh Grant. Durata 113 minuti. Distribuito da 01 Distribution, Amazon Prime Video.',
-                         2020,
-                         'Azione, Commedia',
-                         113);
-//var_dump($secondMovie);
 ?>
--- index.php
+++ index.php
@@ -5,6 +5,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;800&family=Open+Sans:wght@300;400;500;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
   <title>Movies-OOP</title>
 </head>
 <?php
@@ -12,31 +15,23 @@ include __DIR__ .'/data.php';
 ?>
 <body>
   <main class="main__content">
+    <h1>movies</h1>
     <div class="movies__wrapper">
-      <div class="card__movie">
-        <img src="<?php echo $firstMovie->poster ?>" alt="">
-        <h3><?php echo $firstMovie->title ?></h3>
-        <p><?php echo $firstMovie->description ?></p>
-        <span><strong>Genere: </strong> <?php echo $firstMovie->genre ?></span><br>
-        <span><strong>Anno: </strong> <?php echo $firstMovie->year ?></span><br>
-        <span><strong>Durata: </strong> <?php echo $firstMovie->length ?></span>
-      </div>
-      <div class="card__movie">
-        <img src="<?php echo $secondMovie->poster ?>" alt="">
-        <h3><?php echo $secondMovie->title ?></h3>
-        <p><?php echo $secondMovie->description ?></p>
-        <span><strong>Genere: </strong> <?php echo $secondMovie->genre ?></span><br>
-        <span><strong>Anno: </strong> <?php echo $secondMovie->year ?></span><br>
-        <span><strong>Durata: </strong> <?php echo $secondMovie->length ?></span>
-      </div>
-      <div class="card__movie">
-        <img src="<?php echo $thirdMovie->poster ?>" alt="">
-        <h3><?php echo $thirdMovie->title ?></h3>
-        <p><?php echo $thirdMovie->description ?></p>
-        <span><strong>Genere: </strong> <?php echo $thirdMovie->genre ?></span><br>
-        <span><strong>Anno: </strong> <?php echo $thirdMovie->year ?></span><br>
-        <span><strong>Durata: </strong> <?php echo $thirdMovie->length ?></span>
-      </div>
+      <?php 
+      foreach ($movies as $movie) {?>
+        <div class="card__movie">
+          <img src="<?= $movie->poster ?>" alt="">
+          <div class="card__text">
+            <h3><?= $movie->title ?></h3>
+            <p><?= $movie->description ?></p>
+            <span><strong>Genere: </strong> <?= $movie->genre ?></span><br>
+            <span><strong>Anno: </strong> <?= $movie->year ?></span><br>
+            <span><strong>Durata: </strong> <?= $movie->length ?> min</span>
+          </div>
+        </div>
+      <?php    
+      }     
+      ?>
     </div> 
   </main>
   
