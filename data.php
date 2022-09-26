8131beacca7985ec204e0a4af42fbd7b56d379c0
FabioLongoni <longoni.fabio90@gmail.com>
2022-09-26 15:54:35 +0200

    check

--- data.php
+++ data.php
@@ -2,13 +2,14 @@
 
 include __DIR__.'/movie.php';
 
+
 $firstMovie = new Movie('https://mr.comingsoon.it/imgdb/locandine/big/45068.jpg',
                         'The Wolf of Wall Street',
                         'The Wolf of Wall Street è un film di genere drammatico del 2013, diretto da Martin Scorsese, con Leonardo DiCaprio e Jonah Hill. Uscita al cinema il 23 gennaio 2014. Durata 179 minuti. Distribuito da 01 Distribution.',
                         2013,
                         'Drammatico',
                         179);
-var_dump($firstMovie);
+//var_dump($firstMovie);
 
 $secondMovie = new Movie('https://scale.coolshop-cdn.com/product-media.coolshop-cdn.com/AE49RZ/86269df3a5da4519975656c7b9a334a2.jpg/f/inglourious-basterds-quentin-tarantino-dvd.jpg',
                          'Inglourious Basterds',
@@ -16,5 +17,13 @@ $secondMovie = new Movie('https://scale.coolshop-cdn.com/product-media.coolshop-
                          2009,
                          'Guerra, Avventura',
                          148);
-var_dump($secondMovie);
+//var_dump($secondMovie);
+
+$thirdMovie = new Movie('https://pad.mymovies.it/filmclub/2019/07/031/locandina.jpg',
+                         'The Gentlemen',
+                         'The Gentlemen è un film di genere azione, commedia del 2020, diretto da Guy Ritchie, con Matthew McConaughey e Hugh Grant. Durata 113 minuti. Distribuito da 01 Distribution, Amazon Prime Video.',
+                         2020,
+                         'Azione, Commedia',
+                         113);
+//var_dump($secondMovie);
 ?>
--- index.php
+++ index.php
@@ -4,27 +4,41 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="stylesheet" href="./style.css">
+  <title>Movies-OOP</title>
 </head>
 <?php
 include __DIR__ .'/data.php';
 ?>
 <body>
-  <div class="movies__wrapper">
-    <div class="card__movie">
-      <img src="<?php echo $firstMovie->poster ?>" alt="">
-      <h3><?php echo $firstMovie->title ?></h3>
-      <p><?php echo $firstMovie->description ?></p>
-      <span><?php echo $firstMovie->genre ?></span>
-      <span><?php echo $firstMovie->year ?></span>
-    </div>
-    <div class="card__movie">
-      <img src="<?php echo $secondMovie->poster ?>" alt="">
-      <h3><?php echo $secondMovie->title ?></h3>
-      <p><?php echo $secondMovie->description ?></p>
-      <span><?php echo $secondMovie->genre ?></span>
-      <span><?php echo $secondMovie->year ?></span>
-    </div>
-  </div> 
+  <main class="main__content">
+    <div class="movies__wrapper">
+      <div class="card__movie">
+        <img src="<?php echo $firstMovie->poster ?>" alt="">
+        <h3><?php echo $firstMovie->title ?></h3>
+        <p><?php echo $firstMovie->description ?></p>
+        <span><strong>Genere: </strong> <?php echo $firstMovie->genre ?></span><br>
+        <span><strong>Anno: </strong> <?php echo $firstMovie->year ?></span><br>
+        <span><strong>Durata: </strong> <?php echo $firstMovie->length ?></span>
+      </div>
+      <div class="card__movie">
+        <img src="<?php echo $secondMovie->poster ?>" alt="">
+        <h3><?php echo $secondMovie->title ?></h3>
+        <p><?php echo $secondMovie->description ?></p>
+        <span><strong>Genere: </strong> <?php echo $secondMovie->genre ?></span><br>
+        <span><strong>Anno: </strong> <?php echo $secondMovie->year ?></span><br>
+        <span><strong>Durata: </strong> <?php echo $secondMovie->length ?></span>
+      </div>
+      <div class="card__movie">
+        <img src="<?php echo $thirdMovie->poster ?>" alt="">
+        <h3><?php echo $thirdMovie->title ?></h3>
+        <p><?php echo $thirdMovie->description ?></p>
+        <span><strong>Genere: </strong> <?php echo $thirdMovie->genre ?></span><br>
+        <span><strong>Anno: </strong> <?php echo $thirdMovie->year ?></span><br>
+        <span><strong>Durata: </strong> <?php echo $thirdMovie->length ?></span>
+      </div>
+    </div> 
+  </main>
+  
 </body>
 </html>
